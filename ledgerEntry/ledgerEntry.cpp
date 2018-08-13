f52e91cce6f5d4c789957e87da47c13e3eaa05ff
Volentix <sylvain@volentix.com>
2018-08-13 13:01:01 -0400

    - record transfer - print amount

--- ledgerEntry/ledgerEntry.cpp
+++ ledgerEntry/ledgerEntry.cpp
@@ -1,39 +1,68 @@
 #include <eosiolib/eosio.hpp>
+
 using namespace eosio;
 
-class LedgerEntry : public eosio::contract {
-public:
-      using contract::contract;
-      LedgerEntry(account_name n):contract(n) , _ledger_entry(){
-    	  
-      }
-      /// @abi action 
-      void rcrdtrf( account_name s, std::string fromAccount, std::string toAccount, uint64_t fromKey, uint64_t toKey, uint64_t amount) {
-               print( "Hello, ", name{s},' ' , fromAccount, toAccount, fromKey, toKey, amount);
-      }
-        
+class Ledger : public contract {
+  public:
+      Ledger(account_name s):contract(s), ledger(s, s)
+      {}
+      /// @abi action
+      void getrcrd(uint64_t tokey)
+      {
+          for(auto& item : ledger)
+          {
+              if (item.toKey == tokey)
+              {
+
+                print("Amount of VTX: ", item.amount, "\n");
+
+              }
+
+          }
+          print("Volentix Ledger version  0.01");
+      };
+
+      /// @abi action
+      void rcrdtfr(account_name s, std::string fromaccount, std::string toaccount, uint64_t fromkey, uint64_t tokey, uint32_t amount )
+      {
+          //TODO:require_auth(s);
+          print("Add entry ", fromaccount, toaccount, tokey, fromkey, amount);
+          ledger.emplace(get_self(), [&](auto& p)
+                                      {
+                                        p.key = ledger.available_primary_key();
+                                        p.Id = ledger.available_primary_key();
+                                        p.fromAccount = fromaccount;
+                                        p.toAccount = toaccount;
+                                        p.toKey = tokey;
+                                        p.fromKey = fromkey;
+                                        p.amount = amount;
+                                      });
+      };
+
+
   private:
 
       /// @abi table
-	struct ledgerEntry 
-	{
-	   uint64_t     fromKey; 
-	   uint64_t     toKey;
-	   std::string  fromAccount; 
-	   std::string  toAccount; 
-	   uint128_t    amount;
-	   uint64_t by_toKey() const {return toKey; }
-	   auto primary_key() const { return toKey; }
-	   std::string get_from_account() const { return fromAccount; }
-	   std::string get_to_account() const { return toAccount; }
-	   uint128_t get_amount() const { return amount; }
-	  
-	   EOSLIB_SERIALIZE( ledgerEntry, ( fromKey )( toKey )( fromAccount )( toAccount ) )
-	   
-	};
-	typedef eosio::multi_index<N(toKey), ledgerEntry, indexed_by<N(toKey), const_mem_fun<ledgerEntry, uint64_t, &ledgerEntry::by_toKey>>> ledger_entry;
-	
-	ledgerEntry _ledger_entry;
+      struct entry
+      {
+        uint64_t      key; // primary key
+        uint64_t      Id; // second key, non-unique
+        std::string   fromAccount;
+        std::string   toAccount;
+        uint64_t      toKey = 0 ;
+        uint64_t      fromKey = 0;
+        uint32_t      amount = 0;
+
+        uint64_t primary_key() const { return key; }
+        uint64_t by_Id() const {return Id; }
+      };
+      typedef eosio::multi_index<N(entry), entry, indexed_by<N(Id), const_mem_fun<entry, uint64_t, &entry::by_Id>>> ledgertable;
+
+
+
+      ledgertable ledger;
+
 };
 
-EOSIO_ABI( LedgerEntry, (rcrdtrf) )
+
+EOSIO_ABI( Ledger, (getrcrd)(rcrdtfr))
