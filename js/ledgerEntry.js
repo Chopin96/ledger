8b30931ebdac8fbb3eaf9ca938d47e0957d4710f
sylvain <sylvaincormier@protonmail.com>
2018-08-20 15:00:54 -0400

    new address for test

--- js/ledgerEntry.js
+++ js/ledgerEntry.js
@@ -2,7 +2,7 @@ Eos = require('eosjs')
 let Ledger = require('./vltxldgr');
 
 
-config = Ledger.config('cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f', '5KfpCFGR8SBZ3At7oGTDcHgzXgCZRGV6hCT7DTfReYQ63gi3gQz', 'http://127.0.0.1:8888', 60, true, true, true );
+config = Ledger.config('cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f', '5KfpCFGR8SBZ3At7oGTDcHgzXgCZRGV6hCT7DTfReYQ63gi3gQz', 'http://ec2-35-183-54-128.ca-central-1.compute.amazonaws.com:8888', 60, true, true, true);
 
 ledger = Eos(config)
 
--- js/vltxldgr.js
+++ js/vltxldgr.js
@@ -1,33 +1,29 @@
-
-
-exports.config = function(chainid, keyprovider, httpendpoint, expire, broadcast, verbose, sign) {
-	config = {
-  		'chainId': chainid,
-  		'keyProvider': keyprovider,
-  		'httpEndpoint': httpendpoint,
-  		'expireInSeconds': expire,
-  		'broadcast': broadcast,
-  		'verbose': verbose,
-  		'sign': sign,
-	}
-	return config;
+exports.config = function (chainid, keyprovider, httpendpoint, expire, broadcast, verbose, sign) {
+    config = {
+        'chainId': chainid,
+        'keyProvider': keyprovider,
+        'httpEndpoint': httpendpoint,
+        'expireInSeconds': expire,
+        'broadcast': broadcast,
+        'verbose': verbose,
+        'sign': sign,
+    }
+    return config;
 }
 
-exports.rcrdtrf = function(eos, contract, fromaccount, toaccount, fromkey, tokey, amount){
+exports.rcrdtrf = function (eos, contract, fromaccount, toaccount, fromkey, tokey, amount) {
     eos.contract('test').then(test => test.rcrdtrf(
-	{
-    "s": contract,
-    "fromAccount": fromaccount,
-    "toAccount": toaccount,
-    "fromKey": fromkey,
-    "toKey": tokey,
-    "amount": amount,
-    },
-	{
-	 authorization: [ `test@active` ]
-	}
-	).catch())
- }
-
-
+        {
+            "s": contract,
+            "fromAccount": fromaccount,
+            "toAccount": toaccount,
+            "fromKey": fromkey,
+            "toKey": tokey,
+            "amount": amount,
+        },
+        {
+            authorization: [`test@active`]
+        }
+    ).catch())
+}
 
