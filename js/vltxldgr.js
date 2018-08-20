1342bb1bf8c16fd35944af3f8b3e34ca72f0f85d
sylvain <sylvaincormier@protonmail.com>
2018-08-20 10:59:08 -0400

    proper javascript

---
+++ js/vltxldgr.js
@@ -0,0 +1,33 @@
+
+
+exports.config = function(chainid, keyprovider, httpendpoint, expire, broadcast, verbose, sign) {
+	config = {
+  		'chainId': chainid,
+  		'keyProvider': keyprovider,
+  		'httpEndpoint': httpendpoint,
+  		'expireInSeconds': expire,
+  		'broadcast': broadcast,
+  		'verbose': verbose,
+  		'sign': sign,
+	}
+	return config;
+}
+
+exports.rcrdtrf = function(eos, contract, fromaccount, toaccount, fromkey, tokey, amount){
+    eos.contract('test').then(test => test.rcrdtrf(
+	{
+    "s": contract,
+    "fromAccount": fromaccount,
+    "toAccount": toaccount,
+    "fromKey": fromkey,
+    "toKey": tokey,
+    "amount": amount,
+    },
+	{
+	 authorization: [ `test@active` ]
+	}
+	).catch())
+ }
+
+
+
