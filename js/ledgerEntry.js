3b0eeb5e85ea725e92d255030ca33f654dbd3976
realrhys <rhys@relatid.com>
2018-08-24 15:51:56 -0400

    Cleanup

--- js/ledgerEntry.js
+++
@@ -1,9 +0,0 @@
-Eos = require('eosjs')
-let Ledger = require('./vltxldgr');
-
-
-config = Ledger.config('cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f', '5KfpCFGR8SBZ3At7oGTDcHgzXgCZRGV6hCT7DTfReYQ63gi3gQz', 'http://ec2-35-183-54-128.ca-central-1.compute.amazonaws.com:8888', 60, true, true, true);
-
-ledger = Eos(config)
-
-Ledger.rcrdtrf(ledger, "test", "Distribution", "Trust", 1234, 1234, 1234);
