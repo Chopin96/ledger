8f8d1b6fcd48fddb5fafe371f6c10e691c8b4ca7
sylvain <sylvaincormier@protonmail.com>
2018-08-31 09:43:11 -0400

    Strings for 2 more parameters

--- vtxledger/vtxledger.cpp
+++ vtxledger/vtxledger.cpp
@@ -1,6 +1,8 @@
 #include <eosiolib/eosio.hpp>
+#include <fc/crypto/rand.hpp>
 
 using namespace eosio;
+
 
 class Ledger : public contract {
   public:
@@ -12,18 +14,19 @@ class Ledger : public contract {
           uint64_t amount = 0;
           for(auto& item : ledger)
           {
-              if (item.toKey == tokey)
-              {
-                amount += item.amount;
-              }
+              //if (item.toKey == tokey)
+             // {
+               // amount += item.amount;
+             // }
           }
           print("Amount of VTX: ", amount, "\n");
           print("Volentix Ledger version  0.01");
       };
 
       /// @abi action
-      void rcrdtfr(account_name s, std::string fromaccount, std::string toaccount, uint64_t fromkey, uint64_t tokey, uint32_t amount )
+      void rcrdtfr(account_name s, std::string fromaccount, std::string toaccount, std::string fromkey, std::string tokey, uint32_t amount )
       {
+      
           //require_auth(s);
           print("Add entry ", fromaccount, toaccount, tokey, fromkey, amount);
           ledger.emplace(get_self(), [&](auto& p)
@@ -35,27 +38,31 @@ class Ledger : public contract {
                                         p.toKey = tokey;
                                         p.fromKey = fromkey;
                                         p.amount = amount;
+                                        p.lut.insert(std::make_pair(tokey, (uint64_t)&buf));
                                       });
+          
+         
+          
       };
   private:
 
       /// @abi table
       struct entry
       {
-        uint64_t      key; // primary key
-        uint64_t      Id; // second key, non-unique
+        uint64_t      key; 
+        uint64_t      Id; 
         std::string   fromAccount;
         std::string   toAccount;
-        uint64_t      toKey = 0 ;
-        uint64_t      fromKey = 0;
-        uint32_t      amount = 0;
-
+        std::string   toKey;
+        std::string   fromKey;
+        uint32_t      amount;
+        std::map<std::string, uint64_t> lut;
         uint64_t primary_key() const { return key; }
         uint64_t by_Id() const {return Id; }
       };
       typedef eosio::multi_index<N(entry), entry, indexed_by<N(Id), const_mem_fun<entry, uint64_t, &entry::by_Id>>> ledgertable;
 
-
+      
 
       ledgertable ledger;
 
