b48517d88606f732beb2a9eabb3bcd7d2620dbce
Sylvain Cormier <sylvaincormier@protonmail.com>
2018-09-04 14:53:52 -0400

    tokey conversion to uint64

--- vtxledger/vtxledger.cpp
+++ vtxledger/vtxledger.cpp
@@ -1,4 +1,6 @@
 #include <eosiolib/eosio.hpp>
+#include <vector>
+#include<tuple>
 
 using namespace eosio;
 
@@ -8,15 +10,16 @@ class Ledger : public contract {
       Ledger(account_name s):contract(s), ledger(s, s)
       {}
       /// @abi action
-      void getrcrd(uint64_t tokey)
+      void getrcrd(std::string tokey)
       {
+          uint64_t lKey = string_to_name(tokey.c_str());
           uint64_t amount = 0;
           for(auto& item : ledger)
           {
-              //if (item.toKey == tokey)
-             // {
-               // amount += item.amount;
-             // }
+             if (item.toKey == lKey)
+             {
+               amount += item.amount;
+             }
           }
           print("Amount of VTX: ", amount, "\n");
           print("Volentix Ledger version  0.01");
@@ -25,7 +28,7 @@ class Ledger : public contract {
       /// @abi action
       void rcrdtfr(account_name s, std::string fromaccount, std::string toaccount, std::string fromkey, std::string tokey, uint32_t amount )
       {
-      
+          uint64_t lKey = string_to_name(tokey.c_str());
           //require_auth(s);
           print("Add entry ", fromaccount, toaccount, tokey, fromkey, amount);
           ledger.emplace(get_self(), [&](auto& p)
@@ -34,16 +37,20 @@ class Ledger : public contract {
                                         p.Id = ledger.available_primary_key();
                                         p.fromAccount = fromaccount;
                                         p.toAccount = toaccount;
-                                        p.toKey = tokey;
+                                        p.toKey = lKey;
                                         p.fromKey = fromkey;
                                         p.amount = amount;
-                                       // p.lut.insert(std::make_pair(tokey, (uint64_t)&buf));
+                                       
+                                                                   
                                       });
           
          
           
       };
   private:
+     
+    
+
 
       /// @abi table
       struct entry
@@ -52,17 +59,15 @@ class Ledger : public contract {
         uint64_t      Id; 
         std::string   fromAccount;
         std::string   toAccount;
-        std::string   toKey;
+        uint64_t   toKey;
         std::string   fromKey;
         uint32_t      amount;
-        //std::map<std::string, uint64_t> lut;
         uint64_t primary_key() const { return key; }
         uint64_t by_Id() const {return Id; }
       };
       typedef eosio::multi_index<N(entry), entry, indexed_by<N(Id), const_mem_fun<entry, uint64_t, &entry::by_Id>>> ledgertable;
 
-      
-
+     
       ledgertable ledger;
 
 };
